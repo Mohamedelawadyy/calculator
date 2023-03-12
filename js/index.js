4667b080f4bcddced130b12e08185a722ab32246
Mohamedelawadyy <109254580+Mohamedelawadyy@users.noreply.github.com>
2023-03-12 18:02:48 +0300

    upload project

---
+++ js/index.js
@@ -0,0 +1,43 @@
+keys = document.querySelectorAll(" #calculator span");
+operators = ["+", "-", "÷", "x"];
+let point="."
+
+for (let index = 0; index < keys.length; index++) {
+  keys[index].onclick = function () {
+    output = document.querySelector(".screen");
+    outputValue = output.innerHTML;
+    btnValue = this.innerHTML;
+    
+    if (btnValue == "C") {
+      output.innerHTML = "";
+    }
+    if(output.innerHTML.endsWith(".")&& btnValue=="."){
+      console.log("eee");
+      output.innerHTML= outputValue.slice(0, -1);  
+    }
+    else if (btnValue == "D") {
+      output.innerHTML= outputValue.slice(0, -1);      
+    }
+    
+    else if (operators.indexOf(btnValue) > -1) {
+      console.log(btnValue);
+      lastChar = outputValue[outputValue.length - 1];
+      console.log(lastChar);
+      if (outputValue != " " && operators.indexOf(lastChar) == -1) {
+        output.innerHTML += btnValue;
+      }
+    
+      if (outputValue == " " && operators.indexOf(btnValue) == 1) {
+        output.innerHTML += btnValue;
+      }
+    } else if (btnValue == " = ") {
+      equation = outputValue;
+      equation = equation.replace(/x/g, "*").replace(/÷/g, "/");
+      if (equation) {
+        output.innerHTML = eval(equation);
+      }
+    } else {
+      output.innerHTML += btnValue;
+    }
+  };
+}
