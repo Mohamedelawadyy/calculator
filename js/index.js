c035697246cd74d39978c080d631e145f52595fe
Mohamedelawadyy <109254580+Mohamedelawadyy@users.noreply.github.com>
2023-04-10 01:59:50 +0300

    update the "C" buttons

--- js/index.js
+++ js/index.js
@@ -1,32 +1,27 @@
 keys = document.querySelectorAll(" #calculator span");
 operators = ["+", "-", "÷", "x"];
-let point="."
+let point = ".";
 
 for (let index = 0; index < keys.length; index++) {
   keys[index].onclick = function () {
     output = document.querySelector(".screen");
     outputValue = output.innerHTML;
     btnValue = this.innerHTML;
-    
-    if (btnValue == "C") {
-      output.innerHTML = "";
-    }
-    if(output.innerHTML.endsWith(".")&& btnValue=="."){
-      console.log("eee");
-      output.innerHTML= outputValue.slice(0, -1);  
-    }
-    else if (btnValue == "D") {
-      output.innerHTML= outputValue.slice(0, -1);      
-    }
-    
-    else if (operators.indexOf(btnValue) > -1) {
+
+    if (output.innerHTML.endsWith(".") && btnValue == ".") {
+      output.innerHTML = outputValue.slice(0, -1);
+    } else if (btnValue == "C") {
+      output.innerHTML = " ";
+    } else if (btnValue == "D") {
+      output.innerHTML = outputValue.slice(0, -1);
+    } else if (operators.indexOf(btnValue) > -1) {
       console.log(btnValue);
       lastChar = outputValue[outputValue.length - 1];
       console.log(lastChar);
       if (outputValue != " " && operators.indexOf(lastChar) == -1) {
         output.innerHTML += btnValue;
       }
-    
+
       if (outputValue == " " && operators.indexOf(btnValue) == 1) {
         output.innerHTML += btnValue;
       }
